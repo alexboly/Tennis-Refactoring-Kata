a5e8c2bd96876d4044c2e8762aee0cddf1c6b836
Alexandru Bolboaca <alexboly@gmail.com>
2016-11-28 13:15:41 +0200

    Clean up and improve .gitignore

--- cpp/cmake-build-debug/CMakeFiles/cpp.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/cpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
